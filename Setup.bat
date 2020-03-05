ac9edcf404e5db674b7f0621d636e84566e0a843
Rin <onochanbasket@gmail.com>
2020-03-05 12:45:41 +0900

    remove

--- Setup.bat
+++
@@ -1,2 +0,0 @@
-pip3 instal selenium || pip install selenium 
-pip3 install chromedriver-binary==80.0.3987.106.0 || pip install chromedriver-binary==80.0.3987.106.0
--- Setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pip3 instal selenium || pip install selenium 
-pip3 install chromedriver-binary==80.0.3987.106.0 || pip install chromedriver-binary==80.0.3987.106.0
